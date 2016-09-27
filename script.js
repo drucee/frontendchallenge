945839696e3c18d8f41edec23ed759858da764f0
Evan Druce <drucee@gmail.com>
2016-09-27 12:50:01 -0400

    begin writing jquery

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  var root = 'http://jsonplaceholder.typicode.com';
+
+  $.ajax({
+    url: root + '/users/1',
+    method: 'GET'
+  }).then(function(data) {
+    console.log(data);
+});
+  $.ajax({
+    url: root + '/users/2',
+    method: 'GET'
+  }).then(function(data) {
+    console.log(data);
+  });
+});
